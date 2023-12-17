11ab3a0593af4f1501d9509c954a228ee369212f
Dominika Dębska <kanaszewskadominika@gmail.com>
2023-12-17 22:50:29 +0100

    Change of contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-// import {
-//   FaGithub,
-//   FaLinkedin
-// } from 'react-icons/fa';
-// import { BsFillPersonLinesFill } from 'react-icons/bs';
+import copy from 'clipboard-copy';
+import { FaPhoneSquareAlt } from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
+
 import Logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
 import { useState } from 'react';
 
 import '../styles/Navbar.css';
-// import Resume from '../assets/resume.pdf';
 
 const Navbar = () => {
 
   const [menuActive, setMenuActive] = useState(false);
+  const [copyMessage, setCopyMessage] = useState('');
+  const [copyMessagePosition, setCopyMessagePosition] = useState({ x: 0, y: 0 });
 
   const handleMouseEnter = () => {
     setMenuActive(true);
@@ -22,6 +22,27 @@ const Navbar = () => {
   const handleMouseLeave = () => {
     setMenuActive(false);
   };
+
+  const handleCopy = (text, e) => {
+    copy(text);
+    setCopyMessage(`Copied: ${text}`);
+    setCopyMessagePosition({ x: e.clientX, y: e.clientY });
+    setTimeout(() => setCopyMessage(''), 1500);
+  };
+
+  const handleMouseMoveContact = (e) => {
+    setCopyMessagePosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseEnterContact = (e) => {
+    setCopyMessage('Click to copy');
+    setCopyMessagePosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseLeaveContact = () => {
+    setCopyMessage('');
+  };
+  
 
   return (
     <div className='navbar'>
@@ -49,40 +70,28 @@ const Navbar = () => {
           <Link to='contact'>Contact</Link>
         </li>
       </ul>
-      {/* <div className='social-icons'>
+      <div className='social-icons' onMouseMove={handleMouseMoveContact}>
         <ul>
-          <li className='icon'>
-            <a
-              className='font'
-              href="https://www.linkedin.com/in/dominika-d%C4%99bska-kanaszewska-660095154/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Linkedin <FaLinkedin/>
-            </a>
+          <li className='phone-icon' 
+          onClick={(e) => handleCopy('+48 501 497 824', e)}
+          onMouseEnter={handleMouseEnterContact}
+          onMouseLeave={handleMouseLeaveContact}>
+            <span>+48 501 497 824 </span> <FaPhoneSquareAlt />
           </li>
-          <li className='icon'>
-            <a
-              className='font'
-              href="https://github.com/kanaszewska?tab=repositories"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Github <FaGithub/>
-            </a>
-          </li>
-          <li className='icon'>
-            <a
-              className='font'
-              href={Resume}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Resume <BsFillPersonLinesFill/>
-            </a>
-          </li>
+          <li className='mail-icon' 
+            onClick={(e) => handleCopy('kanaszewskadominika@gmail.com', e)}
+          onMouseEnter={handleMouseEnterContact}
+          onMouseLeave={handleMouseLeaveContact}>
+              <span>kanaszewskadominika@gmail.com </span>
+              <FaEnvelope />
+            </li>
         </ul>
-      </div> */}
+        {copyMessage && (
+        <div className='copy-message' style={{ top: `${copyMessagePosition.y}px`, left: `${copyMessagePosition.x}px` }}>
+          {copyMessage}
+        </div>
+      )}
+      </div>
     </div>
   );
 };
